64433b4b358606bc5a4f544d62e704c63708bc66
ITigor1985 <xromer85@gmail.com>
2022-01-26 10:40:52 +0200

    123

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    const width = document.body.clientWidth;
-    if (width >= 768 && width <= 1440) {
-      $('.work__list').slick({
+    
+    
+      $('.reviews__list').slick({
         infinite: false,
         dots: false,
-        arrows: false,
-        slidesToShow: 5,
-        slidesToScroll: 5
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
       });
-    }
+    
   });
