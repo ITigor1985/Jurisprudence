974fbd446240a548086f7e2e52d1496152da303f
ITigor1985 <81867285+ITigor1985@users.noreply.github.com>
2022-01-26 13:45:54 +0000

    Deploying to gh-pages from @ ITigor1985/Jurisprudence@86eab45e72aed7f551ee1dfc2e9296ea4879cf5c 🚀

--- src.ef69c1b3.js
+++ src.ef69c1b3.js
@@ -19,4 +19,4 @@ $(document).ready(function(){$(".reviews__list").slick({infinite:!1,dots:!1,arro
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("material-icons/iconfont/filled.css"),require("aos/dist/aos.css"),require("./sass/main.scss"),require("./js/aos.js"),require("./js/faq.js"),require("./js/backdrop"),require("./js/slick");
 },{"material-icons/iconfont/filled.css":"N8cg","aos/dist/aos.css":"cOFZ","./sass/main.scss":"clu1","./js/aos.js":"Spyv","./js/faq.js":"DnZU","./js/backdrop":"dE7Y","./js/slick":"cDEs"}]},{},["Focm"], null)
-//# sourceMappingURL=src.49f89953.js.map
+//# sourceMappingURL=src.ef69c1b3.js.map
