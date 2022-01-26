c2818466ace9efac16c79d12a7b4532e94dbb154
ITigor1985 <xromer85@gmail.com>
2022-01-26 14:42:50 +0200

    123

--- src/js/slick.js
+++ src/js/slick.js
@@ -5,8 +5,22 @@ $(document).ready(function () {
         infinite: false,
         dots: false,
         arrows: true,
+        infinite: true,
+        centerMode: true,
+        centerPadding: '250px',
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerPadding: '0px',
+            }
+          },
+          
+        ]
       });
     
   });
