01dfd300c8a4ffb202365c1f44c589db5d5ecc29
ITigor1985 <xromer85@gmail.com>
2022-01-26 15:29:10 +0200

    123

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,26 +1,22 @@
 $(document).ready(function () {
-    
-    
-      $('.reviews__list').slick({
-        infinite: false,
-        dots: false,
-        arrows: true,
-        infinite: true,
-        centerMode: true,
-        centerPadding: '250px',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              centerPadding: '0px',
-            }
-          },
-          
-        ]
-      });
-    
+  $('.reviews__list').slick({
+    infinite: false,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '350px',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '0px',
+        },
+      },
+    ],
   });
+});
