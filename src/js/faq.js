a88627ab87093ef252733eb960a3cc4353518cc1
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-01-31 17:19:27 +0200

    123

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,8 +1,17 @@
+const modalForm = document.querySelector('[data-modal-form]');
+const openForm = document.querySelector('.answer__button');
+const closeForm = document.querySelector('[data-modal-close]');
+
 const faqs = document.querySelectorAll('.faq');
 faqs.forEach(faq => {
   faq.addEventListener('click', faqActive);
+
   function faqActive(e) {
-    console.log(e.target);
-    if (e.target !== this) faq.classList.toggle('active');
+    console.log(e.target.nodeName);
+    if (e.target.nodeName !== 'BUTTON') faq.classList.toggle('active');
+  }
+
+  function toggleModal() {
+    modalForm.classList.toggle('is-shown');
   }
 });
