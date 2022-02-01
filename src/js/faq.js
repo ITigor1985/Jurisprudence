94357b2fa7648f62008de3390b56a6aa63d8e676
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-02-01 12:14:56 +0200

    123

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,31 +1,33 @@
-const modalForm = document.querySelector('[data-modal-form]');
-const openForm = document.querySelector('.header-mid__button');
-const closeForm = document.querySelector('[data-modal-form-close]');
+(() => {
+  const modalForm = document.querySelector('[data-modal-form]');
+  const openForm = document.querySelector('.header-mid__button');
+  const closeForm = document.querySelector('[data-modal-form-close]');
 
-openForm.addEventListener('click', openModal);
-function openModal() {
-  modalForm.classList.add('is-shown');
-}
-closeForm.addEventListener('click', closeModal);
+  openForm.addEventListener('click', openModal);
+  function openModal() {
+    modalForm.classList.add('is-shown');
+  }
+  closeForm.addEventListener('click', closeModal);
 
-function closeModal() {
-  modalForm.classList.remove('is-shown');
-}
+  function closeModal() {
+    modalForm.classList.remove('is-shown');
+  }
 
-const faqs = document.querySelectorAll('.faq');
-faqs.forEach(faq => {
-  faq.addEventListener('click', faqActive);
+  const faqs = document.querySelectorAll('.faq');
+  faqs.forEach(faq => {
+    faq.addEventListener('click', faqActive);
 
-  function faqActive(e) {
-    console.log(e.target.nodeName);
-    if (e.target.nodeName !== 'BUTTON') {
-      faq.classList.toggle('active');
-    } else {
-      toggleModal();
+    function faqActive(e) {
+      console.log(e.target.nodeName);
+      if (e.target.nodeName !== 'BUTTON') {
+        faq.classList.toggle('active');
+      } else {
+        toggleModal();
+      }
     }
-  }
 
-  function toggleModal() {
-    modalForm.classList.toggle('is-shown');
-  }
-});
+    function toggleModal() {
+      modalForm.classList.toggle('is-shown');
+    }
+  });
+})();
