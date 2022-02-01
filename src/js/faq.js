86665f6abb00b1a5935f000352b80f5739fae716
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-02-01 09:32:02 +0200

    123

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,6 @@
 const modalForm = document.querySelector('[data-modal-form]');
 const openForm = document.querySelector('.answer__button');
-const closeForm = document.querySelector('[data-modal-close]');
+const closeForm = document.querySelector('[data-modal-form-close]');
 
 const faqs = document.querySelectorAll('.faq');
 faqs.forEach(faq => {
